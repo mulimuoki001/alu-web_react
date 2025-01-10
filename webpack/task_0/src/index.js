c29941b1d058a028494a0e02fcfd205d0b560790
mulimuoki002 <mulimuoki001@gmail.com>
2025-01-10 03:21:38 +0300

    removed webpack

--- webpack/task_0/src/index.js
+++
@@ -1,8 +0,0 @@
-import $ from "jquery";
-
-$(document).ready(()=> {
-    $('body').append('<p>Holberton Dashboard</p>',
-        '<p>Dashboard data for the students</p>',
-        '<p>Copyright - Holberton School</p>'
-    );
-})
