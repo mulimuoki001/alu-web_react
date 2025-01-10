33dbfefd0ed85801cbc9123fc4211ee6ab44b62a
mulimuoki002 <mulimuoki001@gmail.com>
2025-01-10 03:28:05 +0300

    filesmodification

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,8 +1,7 @@
 import $ from "jquery";
 
-$(document).ready(()=> {
-    $('body').append('<p>Holberton Dashboard</p>',
+
+$('body').append('<p>Holberton Dashboard</p>',
         '<p>Dashboard data for the students</p>',
         '<p>Copyright - Holberton School</p>'
-    );
-})
+);
