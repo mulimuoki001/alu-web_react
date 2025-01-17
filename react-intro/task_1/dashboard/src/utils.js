fba8cd33e8e36c735f564aa6b9c7fbaed902ff89
mulimuoki002 <mulimuoki001@gmail.com>
2025-01-17 13:23:27 -0500

    react props

--- react-intro/task_1/dashboard/src/utils.js
+++
@@ -1,9 +0,0 @@
-const getFullYear = () => {
-    return new Date().getFullYear();
-}
-
-const getFooterCopy = (isIndex) => {
-    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-}
-
-export { getFooterCopy, getFullYear };
